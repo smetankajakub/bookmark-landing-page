f2820ba43807819b828a51726c48dc417822f789
xsmetankaj <smetanka321@gmail.com>
2021-11-08 22:52:11 +0100

    talwind config mobile first part concept

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+      extend: {
+        colors: {
+          softblue:{
+            DEFAULT: '#5368df'
+          }
+    }
+  },
+
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
