5ce941f0730c6cd5b71d2b9cba23887164e9d088
xsmetankaj <smetanka321@gmail.com>
2021-11-09 21:28:11 +0100

    make extended colors and new class working, features and extensions part template

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,27 @@ module.exports = {
   theme: {
       extend: {
         colors: {
-          softblue:{
-            DEFAULT: '#5368df'
+          primary: {
+            soft: {
+              blue: '#5368df',
+              red: 'hsl(0, 94%, 66%)'
+            },
+          },
+          neutral: {
+            blue: {
+              grayish: 'hsl(229, 8%, 60%)',
+              dark: 'hsl(229, 31%, 21%)'
+            }
           }
-    }
-  },
-
+        }
+      }
   },
   variants: {
     extend: {},
   },
   plugins: [],
 }
+// --color-primary-soft-blue: #5368df;
+// --color-primary-soft-red: hsl(0, 94%, 66%);
+// --color-neutral-grayish-blue: hsl(229, 8%, 60%);
+// --color-primary-dark-blue: hsl(229, 31%, 21%);
