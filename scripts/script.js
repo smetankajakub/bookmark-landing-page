a131cfd94dfc9024220c55f89ef5cd49586f5f87
xsmetankaj <smetanka321@gmail.com>
2021-11-11 22:37:34 +0100

    faqs collapsible

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,31 @@
 function burgerMenu(){
     let headerOpen = document.getElementById('header-burger-open');
-    let headerClose = document.getElementById('header-burger-closed');
-    let topHeader = document.getElementById('top-header');
     if(headerOpen.classList.contains('hidden')){
         headerOpen.classList.remove('hidden');
-        // headerClose.classList.add('hidden');
 
     } else {
-        // headerClose.classList.remove('hidden');
         headerOpen.classList.add('hidden');
     }
-    console.log('caaaaaal')
+}
+
+function closeHamburger(){
+    let headerOpen = document.getElementById('header-burger-open');
+    headerOpen.classList.add('hidden');
+}
+
+function changeIcon(id){
+    let img = document.getElementById(id);
+    let content = document.getElementById(id + '-ans');
+    
+    if( content.classList.contains('hidden') ){
+
+        content.classList.remove('hidden');
+        img.classList.remove('transform')
+        img.classList.remove('rotate-180')
+    
+    } else {
+        content.classList.add('hidden');
+        img.classList.add('transform')
+        img.classList.add('rotate-180')
+    }
 }
